e9f00fcbf01aedc192cffe4545d37f1a141d7787
yibozhong <yibozhong657@gmail.com>
2025-01-25 21:46:03 +0800

    Add setup.py, without checking

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="flazoo",
+    version="0.1",
+    description="Flash-Linear-Attention models beyond language",
+    author="Yibo Zhong",
+    packages=find_packages(),
+)
