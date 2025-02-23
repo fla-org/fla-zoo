9af7e64f19cd3fb38b6d9d1ac4f930b180d140fe
yibozhong <yibozhong657@gmail.com>
2025-02-23 14:59:21 +0800

    Add impl for LightNet vision model

---
+++ flazoo/models/vision/lightnet/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from transformers import AutoConfig, AutoModel, AutoModelForImageClassification, AutoModelForMaskedImageModeling
+
+from .configuration_lightnet import LightNetVisionConfig
+from .modeling_lightnet import(LightNetVisionModel,
+                                            LightNetForImageClassification,
+                                            LightNetForMaskedImageModeling)
+
+AutoConfig.register(LightNetVisionConfig.model_type, LightNetVisionConfig)
+AutoModel.register(LightNetVisionConfig, LightNetVisionModel)
+AutoModelForImageClassification.register(LightNetVisionConfig, LightNetForImageClassification)
+AutoModelForMaskedImageModeling.register(LightNetVisionConfig, LightNetForMaskedImageModeling)
+
+__all__ = ['LightNetVisionModel', 'LightNetForImageClassification', 'LightNetForMaskedImageModeling', 'LightNetVisionConfig']
