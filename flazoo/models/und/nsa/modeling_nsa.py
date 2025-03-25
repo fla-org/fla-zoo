6226303090b7269209b436ce81fe7a079b761ddc
yibozhong <yibozhong657@gmail.com>
2025-03-25 17:12:00 +0800

    Delete import of full attention in nsa

--- flazoo/models/und/nsa/modeling_nsa.py
+++ flazoo/models/und/nsa/modeling_nsa.py
@@ -18,7 +18,6 @@ from transformers.modeling_outputs import (ImageClassifierOutput,
 from transformers.modeling_utils import PreTrainedModel
 from transformers.utils import logging
 
-from flazoo.models.attentions import VisionAttention
 from flazoo.models.attentions import VisionNativeSparseAttention
 from .configuration_nsa import NSAVisionConfig
 from fla.models.utils import Cache
