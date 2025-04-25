944dd51304682100853106c7f997975fd16c469c
yibozhong <yibozhong657@gmail.com>
2025-04-25 11:14:24 +0800

    Update SigLIP2 init code

--- examples/common/init_from_a_model.py
+++ examples/common/init_from_a_model.py
@@ -20,18 +20,49 @@ def create_model(num_layers, hidden_size):
     config = DeltaNetVisionConfig(**config)
     return DeltaNetVisionModel(config)
 
+# init from dino
+# def main():    
+
+#     model_a = create_model(12, 768)
+#     model_b = AutoModel.from_pretrained('facebook/dinov2-base')
+    
+#     print("Initializing with custom parameter mapping...")
+#     # Example of custom parameter mapping
+#     custom_mapping = {
+#         "attn.q_proj": "attention.attention.query",
+#         "attn.k_proj": "attention.attention.key",
+#         "attn.v_proj": "attention.attention.value",
+#         "attn.o_proj": "attention.output.dense",
+#         "channel_mixer.net.0": "mlp.fc1",
+#         "channel_mixer.net.2": "mlp.fc2"
+#     }
+#     initialize_custom_mapping(
+#         model_a=model_a,
+#         model_b=model_b,
+#         param_mapping=custom_mapping
+#     )
+#     # some random checking
+#     for i in range(12):
+#         tensor1 = model_b.encoder.layer[i].attention.attention.key.weight 
+#         tensor2 = model_a.encoder.blocks[i].attn.k_proj.weight  
+#         # check whether the two tensors are equal
+#         assert torch.equal(tensor1, tensor2)
+#         print(f"Layer {i} initialized successfully!")
+#     print("Initialization complete!")
+
+# init from siglip
 def main():    
 
     model_a = create_model(12, 768)
-    model_b = AutoModel.from_pretrained('facebook/dinov2-base')
+    model_b = AutoModel.from_pretrained('google/siglip2-base-patch16-224').vision_model
     
     print("Initializing with custom parameter mapping...")
     # Example of custom parameter mapping
     custom_mapping = {
-        "attn.q_proj": "attention.attention.query",
-        "attn.k_proj": "attention.attention.key",
-        "attn.v_proj": "attention.attention.value",
-        "attn.o_proj": "attention.output.dense",
+        "attn.q_proj": "self_attn.q_proj",
+        "attn.k_proj": "self_attn.k_proj",
+        "attn.v_proj": "self_attn.v_proj",
+        "attn.o_proj": "self_attn.out_proj",
         "channel_mixer.net.0": "mlp.fc1",
         "channel_mixer.net.2": "mlp.fc2"
     }
@@ -42,7 +73,7 @@ def main():
     )
     # some random checking
     for i in range(12):
-        tensor1 = model_b.encoder.layer[i].attention.attention.key.weight 
+        tensor1 = model_b.encoder.layers[i].self_attn.k_proj.weight 
         tensor2 = model_a.encoder.blocks[i].attn.k_proj.weight  
         # check whether the two tensors are equal
         assert torch.equal(tensor1, tensor2)
