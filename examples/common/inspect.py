8a054d671039fa2fab0dcf1bb1567c2187b86069
yibozhong <yibozhong657@gmail.com>
2025-04-27 14:22:31 +0800

    Update inspect scripts

--- examples/common/inspect.py
+++ examples/common/inspect.py
@@ -4,8 +4,9 @@ from PIL import Image
 import requests
 from flazoo.helpers.informer import log_model_parameters_flat, log_model_parameters
 
-processor = AutoImageProcessor.from_pretrained('facebook/dinov2-base')
-model = AutoModel.from_pretrained('facebook/dinov2-base')
+from transformers import Siglip2ForImageClassification
+# model = AutoModel.from_pretrained('facebook/dinov2-base')
+model = AutoModel.from_pretrained('google/siglip2-base-patch16-224').vision_model
 
 log_model_parameters(model)
 log_model_parameters_flat(model)
