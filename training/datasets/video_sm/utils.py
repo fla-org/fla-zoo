66489de9a6089b63b6aed978f0a02774cd7c2e2b
yibozhong <yibozhong657@gmail.com>
2025-04-28 16:48:14 +0800

    Delete tensorboard import

--- training/datasets/video_sm/utils.py
+++ training/datasets/video_sm/utils.py
@@ -15,8 +15,6 @@ import torch.distributed as dist
 from torch import inf
 import random
 
-from tensorboardX import SummaryWriter
-
 
 class SmoothedValue(object):
     """Track a series of values and provide access to smoothed values over a
@@ -222,30 +220,6 @@ class MetricLogger(object):
         total_time_str = str(datetime.timedelta(seconds=int(total_time)))
         print('{} Total time: {} ({:.4f} s / it)'.format(
             header, total_time_str, total_time / total_len))
-
-
-class TensorboardLogger(object):
-    def __init__(self, log_dir):
-        self.writer = SummaryWriter(logdir=log_dir)
-        self.step = 0
-
-    def set_step(self, step=None):
-        if step is not None:
-            self.step = step
-        else:
-            self.step += 1
-
-    def update(self, head='scalar', step=None, **kwargs):
-        for k, v in kwargs.items():
-            if v is None:
-                continue
-            if isinstance(v, torch.Tensor):
-                v = v.item()
-            assert isinstance(v, (float, int))
-            self.writer.add_scalar(head + "/" + k, v, self.step if step is None else step)
-
-    def flush(self):
-        self.writer.flush()
 
 
 def seed_worker(worker_id):
