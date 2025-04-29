004e635a07ef289c4fd2c2cb3f53f45e16950457
yibozhong <yibozhong657@gmail.com>
2025-04-29 23:27:25 +0800

    Add convenient function for init from another FLA model

--- flazoo/helpers/linearizer.py
+++ flazoo/helpers/linearizer.py
@@ -1,4 +1,4 @@
-from transformers import AutoModel
+from transformers import AutoModel, AutoConfig
 from flazoo.helpers.initializer import (
     initialize_custom_mapping
 )
@@ -18,6 +18,81 @@ For example, SigLIP2 has some extra components like head.mlp.fc1.weight and head
 , which are not in FLA models. So they ignored. However, if you use linearized SigLIP2, you can use these extra components.
 
 """
+
+def init_from_fla_vision_und(
+    fla_model,
+    another_fla_model,
+    train_mlp: bool = True,
+    init_embedding: bool = True,
+    return_pretrained: bool = False,
+):
+    """
+    Initialize a FLA vision und model also from a FLA model \n
+    Note that the two models should have the same architecture.
+
+    Args:
+        fla_model: FLA models to be initialized
+        another_fla_model: FLA models to load
+        train_mlp: Whether to train the MLP layers (default: True)
+        init_embedding: Whether to initialize the embedding layers (default: True)
+        return_pretrained: Whether to return the pretrained model (default: False)
+
+    Returns:
+        Initialized FLA model
+    """
+    # Define parameter mapping
+    param_mapping = {
+        "attn.q_proj": "attn.q_proj",
+        "attn.k_proj": "attn.k_proj",
+        "attn.v_proj": "attn.v_proj",
+        "attn.o_proj": "attn.o_proj",
+        "ln_1": "ln_1",
+        "ln_2": "ln_2",
+        "channel_mixer.net.0": "channel_mixer.net.0",
+        "channel_mixer.net.2": "channel_mixer.net.2"
+    }
+
+    initialize_custom_mapping(
+        model_a=fla_model,
+        model_b=another_fla_model,
+        param_mapping=param_mapping
+    )
+
+    if not train_mlp:
+        for n, p in fla_model.named_parameters():
+            if "channel_mixer" in n:
+                p.requires_grad_(False)
+    
+    if init_embedding:
+        logging.info("Initializing embedding layers, make sure your shapes match.")
+        fla_model.embeddings.patch_embeddings.projection.weight.data.copy_(
+            another_fla_model.embeddings.patch_embeddings.projection.weight.data
+        )
+        fla_model.embeddings.patch_embeddings.projection.bias.data.copy_(
+            another_fla_model.embeddings.patch_embeddings.projection.bias.data
+        )
+        fla_model.embeddings.position_embeddings.data.copy_(
+            another_fla_model.embeddings.position_embeddings.data
+        )
+        assert torch.equal(
+            fla_model.embeddings.patch_embeddings.projection.weight,
+            another_fla_model.embeddings.patch_embeddings.projection.weight
+        )
+        assert torch.equal(
+            fla_model.embeddings.patch_embeddings.projection.bias,
+            another_fla_model.embeddings.patch_embeddings.projection.bias
+        )
+        assert torch.equal(
+            fla_model.embeddings.position_embeddings,
+            another_fla_model.embeddings.position_embeddings
+        )
+    
+    if not return_pretrained:
+        return fla_model
+    else:
+        return fla_model, another_fla_model
+
+
 
 def init_from_dino2_base_p14(
     fla_model,
