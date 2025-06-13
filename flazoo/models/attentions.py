2c6d89884ddf0880a4e74296630f85c48c794950
yibozhong <yibozhong657@gmail.com>
2025-06-14 04:21:04 +0800

    [STA] change assert to warnings

--- flazoo/models/attentions.py
+++ flazoo/models/attentions.py
@@ -1029,11 +1029,12 @@ class SlidingTileAttention2D(nn.Module):
         self.window_size_w = window_size_w
         self.tile_size_h = tile_size_h
         self.tile_size_w = tile_size_w
-
-        # Validate tile size for flex attention requirements
-        assert (self.tile_size_h * self.tile_size_w) % 128 == 0, (
-            f"tile numel {self.tile_size_h * self.tile_size_w} is not divisible by 128, which is required for flex attention"
-        )
+        
+        # change to warning
+        if (self.tile_size_h * self.tile_size_w) % 128 != 0:
+            logging.warning(
+                f"tile numel {self.tile_size_h * self.tile_size_w} is not divisible by 128, which is required for flex attention, you are using a slow version of STA, which is not ideal."
+            )
 
         self.seq_len = seq_len
         self.h_dim = h_dim
